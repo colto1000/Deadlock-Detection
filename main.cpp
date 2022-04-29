627f0fe5c5c1b3994bcdf19195fb8eeb3ed7dd13
NTri-1965 <104516978+NTri-1965@users.noreply.github.com>
2022-04-28 23:16:03 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,7 +3,88 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <set>
+#define node 6
+
+//Sourcing
+//https://www.tutorialspoint.com/Detect-Cycle-in-a-Directed-Graph
+//Simple algorithm I used to determine whether or not the adjacency matrix is cyclic
+
 using namespace std;
+
+	int graph[node][node] =
+	{
+		{1,0,0,0,1,0},
+		{0,1,0,1,0,1},
+		{0,0,1,0,0,0},			//due to timing and differing manners on how to determine whether deadlock was or was not present I simply hardcoded the given graph into the program
+		{1,0,1,1,0,0},			//Colton was in charge of pulling data from the given text, but my other portion was incompatable, thus due to time constraints I had to forgo that portion
+		{0,1,0,0,1,0},
+		{0,0,1,0,0,1}
+	};
+
+bool search(int curr, set<int>&wSet, set<int>&gSet, set<int>&bSet)
+{
+	wSet.erase(wSet.find(curr));		//delete the given current node from the given set
+	gSet.insert(curr);		//insert given current node to differing set
+
+	for(int i = 0; i < node; i++)		//for loop looping through all nodes within adjacency matrix
+	{
+		if(graph[curr][i] != 0)		//given node is withing the given set, continue
+		{
+			if(bSet.find(i) != bSet.end())
+			{
+				continue;
+			}
+			if(gSet.find(i) != gSet.end())		//given node is within the specified set return 1
+			{
+				return true;
+			}
+			if(search(i,wSet,gSet,bSet))		//given node is within the specifiedset return 1
+			{
+				return true;
+			}
+		}
+
+	}
+
+	gSet.erase(gSet.find(curr));		//remove the given current value from the gSet and push it to the given bSet
+	bSet.insert(curr);
+}
+
+bool cyclic()		//function that determines if given graph is cyclic
+{
+	set<int> gSet;		//declaration of given set containers
+
+        set<int> wSet;
+
+        set<int> bSet;
+
+        for(int r = 0; r < node; r++)		//place all elements within the w set
+        {
+                wSet.insert(r);
+        }
+
+        while(wSet.size() > 0)		//while loop iterating through the w set whilst it still obtains elements
+        {
+                for(int curr = 0; curr < node; curr++)		//nested loops determining if all nodes within the given matrix are within the w set, if so call the given search() then return 1
+                {
+                        if(wSet.find(curr) != wSet.end())
+                        {
+                                if(search(curr, wSet, gSet, bSet))
+                                {
+                                        return true;
+                                }
+                        }
+                }
+
+        }
+        return false;
+
+
+}
+
+
+
 
 
 int main()
@@ -50,7 +131,7 @@ int main()
 		ss.str(line);
 		ss >> ws;
 		line = ss.str();
-		
+
 		if (line.at(0) == '%' || line.size() == 0)  // if line is comment or empty
 		{
 			continue;
@@ -111,6 +192,21 @@ int main()
 	}
 	cout << endl;
 	// --- end of debug output ---
+
+	//int curr;
+	//wSet.erase(Wset.find(curr));
+
+
+	bool determine = cyclic();		//setting the given boolean value to the cyclic() call
+
+	if(determine)		//if-else statements determining deadlock
+		{
+			cout << "\nDeadlock Detected\n";
+		}
+	else
+		{
+			cout << "\nDeadlock Not Detected\n";
+		}
 
 	return 0;
 
